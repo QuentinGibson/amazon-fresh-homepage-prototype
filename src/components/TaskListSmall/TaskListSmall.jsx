9538cc18448c0a69d35ca1f6d97b8c5f44d6af94
Quentin Gibson <quentingibson94@gmail.com>
2021-08-10 22:14:44 -0400

    add tags to all data

--- src/components/TaskListSmall/TaskListSmall.jsx
+++ src/components/TaskListSmall/TaskListSmall.jsx
@@ -9,7 +9,7 @@ const TaskListSmall = ({activeStatus}) => {
   const [search, setSearch] = useState('')
   const isFiltered = (task) => {
     const active = (activeStatus !== "All" ? task.category === activeStatus : true)
-    const searched = (task.category.includes(search) || task.desc.includes(search) || task.title.includes(search))
+    const searched = (task.category.includes(search) || task.desc.includes(search) || task.title.includes(search) || task.tags.includes(search))
     if (active) {
       if (search.trim().toLowerCase() !== "") {
         return searched
@@ -18,23 +18,22 @@ const TaskListSmall = ({activeStatus}) => {
     }
     return false
   }
-  const tasks = data.tasks.filter(task => {
+  const taskList = data.tasks.filter(task => {
     return isFiltered(task)
   })
-
-  const taskList = tasks.map((task) => {
-    const { category, title, desc, url } = task;
-    return <Task category={category} title={title} desc={desc} url={url} />;
+    .map((task) => {
+      const { category, title, desc, url } = task;
+      return <Task category={category} title={title} desc={desc} url={url} />;
   });
-    return (
-      <>
-        <div>
-          <Search setSearch={setSearch}/>
-        </div>
-        <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
-          {taskList}
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div>
+        <Search setSearch={setSearch} />
+      </div>
+      <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 gap-8">
+        {taskList}
+      </div>
+    </>
+  );
 }
 export default TaskListSmall;
